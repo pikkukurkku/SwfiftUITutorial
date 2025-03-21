1c1b1e1b7f2de5280e5eef9e918e9e5d37b0fe10
pikkukurkku <natalia.ogorek@gmail.com>
2025-03-21 15:54:45 +0100

    app storage

--- SwiftfulThinkingCourse/OnboardningViews/OnboardingView.swift
+++ SwiftfulThinkingCourse/OnboardningViews/OnboardingView.swift
@@ -18,6 +18,24 @@ struct OnboardingView: View {
      3 - Add gender
      */
     @State var onboardingState: Int = 0
+    let transition: AnyTransition = .asymmetric(
+        insertion: .move(edge: .trailing),
+        removal: .move(edge: .leading))
+    
+    // onboarding inputs
+    @State var name: String = ""
+    @State var age: Double = 50
+    @State var gender: String = ""
+    
+    // for the alert
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+    
+    // app storage
+    @AppStorage("name") var currentUserName: String?
+    @AppStorage("age") var currentUserAge: Int?
+    @AppStorage("gender") var currentUserGender: String?
+    @AppStorage("signed_in") var currentUserSignedIn: Bool = false
     
     var body: some View {
         ZStack  {
@@ -25,20 +43,31 @@ struct OnboardingView: View {
                 switch onboardingState {
                 case 0:
                     welcomeSection
+                        .transition(transition)
+                case 1:
+                    addNameSection
+                        .transition(transition)
+                case 2:
+                    addAgeSection
+                        .transition(transition)
+                case 3:
+                    addGenderSection
+                        .transition(transition)
                 default:
                     RoundedRectangle(cornerRadius: 25.0)
                         .foregroundColor(.green)
                 }
             }
-            
             VStack {
                 Spacer()
                 bottomButton
             }
             .padding(30)
         }
+        .alert(isPresented: $showAlert, content: {
+            return Alert(title: Text(alertTitle))
+        })
     }
-
 }
 
 #Preview {
@@ -50,7 +79,9 @@ struct OnboardingView: View {
 extension OnboardingView {
     
     private var bottomButton: some View {
-        Text("Sign in")
+        Text(onboardingState == 0 ? "SIGN UP" :
+                onboardingState == 3 ? "FINISH" :
+        "NEXT")
             .font(.headline)
             .foregroundColor(.purple)
             .frame(height: 55)
@@ -58,8 +89,8 @@ extension OnboardingView {
             .background(Color.white)
             .cornerRadius(10)
             .onTapGesture {
-                //
-        }
+                handleNextButtonPressed()
+            }
     }
     
     private var welcomeSection: some View {
@@ -89,5 +120,123 @@ extension OnboardingView {
         }
         .multilineTextAlignment(.center)
         .padding()
+    }
+    
+    private var addNameSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("What's your name?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            TextField("Your name here...", text: $name)
+                .font(.headline)
+                .frame(height: 55)
+                .padding(.horizontal)
+                .background(Color.white)
+                .cornerRadius(10)
+            Spacer()
+            Spacer()
+        }
+        .padding()
+    }
+    
+    private var addAgeSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("What's your age?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            
+            Text("\(String(format: "%.0f", age))")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            Slider(value: $age, in: 18...100, step: 1)
+                .accentColor(.white)
+            Spacer()
+            Spacer()
+        }
+        .padding()
+    }
+    
+    
+    private var addGenderSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("What's your gender?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            Text(gender.count > 1 ? gender : "Select a gender")
+            .font(.headline)
+            .foregroundColor(.gray)
+            .padding()
+            .frame(height: 55)
+            .background(Color.white)
+            .cornerRadius(10)
+         
+            Picker(
+                selection: $gender,
+                label: Text(""),
+                content: {
+                    Text("Male").tag("Male")
+                    Text("Female").tag("Female")
+                    Text("Non-Binary").tag("Non-Binary")
+                })
+                .pickerStyle(WheelPickerStyle())
+            Spacer()
+            Spacer()
+        }
+        .padding(30)
+    }
+}
+
+// MARK: FUNCTIONS
+
+extension OnboardingView {
+    
+    func handleNextButtonPressed() {
+        
+        
+        // CHECK INPUTS
+        switch onboardingState {
+        case 1:
+            guard name.count >= 3 else {
+                showAlert(title: "Your name must be at least 3 characters long! 😩")
+                return
+            }
+        case 3:
+            guard gender.count > 1 else {
+                showAlert(title: "Please select a gender before moving forward! 😬")
+                return
+            }
+        default:
+            break
+        }
+        
+        if onboardingState == 3 {
+            signIn()
+        } else {
+        
+            withAnimation(.spring()) {
+                onboardingState += 1
+            }
+        }
+    }
+    
+    func signIn() {
+        currentUserName = name
+        currentUserAge = Int(age)
+        currentUserGender = gender
+        withAnimation(.spring()) {
+            currentUserSignedIn = true
+        }
+    }
+    
+    func showAlert(title: String) {
+        alertTitle = title
+        showAlert.toggle()
     }
 }
